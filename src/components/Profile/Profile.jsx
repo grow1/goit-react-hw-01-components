eada915d9bd0399b5eb5ae2ef4b37c43eca9056c
Yurii Mahdych <yura.magdich@gmail.com>
2023-06-21 01:39:20 +0300

    basic structure, profile jsx ready

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+import './Profile.module.css';
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  followers,
+  views,
+  likes,
+}) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity"> {followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity"> {views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity"> {likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
